cbfc63949d1f2743b45f785cdb07d455d4748403
Teodor Heggelund <git@teod.eu>
2023-03-24 22:51:43 +0100

    histogram

--- src/teodorlu/clerk_hammertime/montecarlo.clj
+++ src/teodorlu/clerk_hammertime/montecarlo.clj
@@ -1,6 +1,7 @@
 (ns teodorlu.clerk-hammertime.montecarlo
   (:refer-clojure :exclude [range])
   (:require
+   [nextjournal.clerk :as clerk]
    [tech.v3.datatype :as d]))
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
@@ -10,3 +11,14 @@
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn sample [N]
   (.toArray (.doubles (java.util.Random.) N)))
+
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn histogram
+  "histogram with automatic buckets, all data is passed to frontend"
+  [values]
+  (clerk/vl {:data {:values (for [v values] {:x v})}
+             :mark :bar
+             :encoding {:x {:bin true
+                            :field :x}
+                        :y {:aggregate :count}}
+             :embed/opts {:actions false}}))
